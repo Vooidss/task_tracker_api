6177ef35df64af4c8515e2fd48d42e07b4e4bd87
Vooidss <gegor433@gmail.com>
2023-11-11 16:28:44 +0300

    hehe-ha

---
+++ src/main/java/may/code/task/tracker/api/Applictation.java
@@ -0,0 +1,15 @@
+package may.code.task.tracker.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Applictation {
+    public static void main(String[] args) {
+
+        SpringApplication
+                .run(Applictation.class,args);
+    }
+}
